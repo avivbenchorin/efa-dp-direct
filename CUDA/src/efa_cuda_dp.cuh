avivbenchorin/efa-dp-direct

commit 25967086c6809a5cc889d728afd8ae88079cb879

Add initial code
Define directory structure and add initial EFA data path CUDA
implementation along with getting-started documentation with code
examples.

Signed-off-by: Michael Margolin <mrgolin@amazon.com>

CUDA/src/efa_cuda_dp.cuh
@@ -1,0 +1,76 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+#ifndef EFA_CUDA_DP_CUH
+#define EFA_CUDA_DP_CUH
+
+#include <cstddef>
+#include <stdint.h>
+#include <cuda_runtime.h>
+#include "efa_cuda_dp.h"
+#include "efa_io_defs.h"
+
+__device__ void *efa_cuda_cq_poll(efa_cuda_cq *cq, uint32_t position);
+
+__device__ int efa_cuda_cq_pop(efa_cuda_cq *cq, uint32_t amount);
+
+__device__ enum efa_cuda_wc_opcode efa_cuda_wc_read_opcode(void *wc_buf);
+
+__device__ bool efa_cuda_wc_is_unsolicited(void *wc_buf);
+
+__device__ uint16_t efa_cuda_wc_read_req_id(void *wc_buf);
+
+__device__ uint32_t efa_cuda_wc_read_vendor_err(void *wc_buf);
+
+__device__ bool efa_cuda_wc_has_imm(void *wc_buf);
+
+__device__ uint32_t efa_cuda_wc_read_imm_data(void *wc_buf);
+
+__device__ uint32_t efa_cuda_wc_read_byte_len(void *wc_buf);
+
+__device__ uint32_t efa_cuda_wc_read_qp_num(void *wc_buf);
+
+__device__ uint32_t efa_cuda_wc_read_src_qp(void *wc_buf);
+
+__device__ uint32_t efa_cuda_wc_read_slid(void *wc_buf);
+
+__device__ int efa_cuda_init_send_wr(void *wr_buf, uint16_t wr_id);
+
+__device__ int efa_cuda_init_send_imm_wr(void *wr_buf, uint16_t wr_id, uint32_t imm_data);
+
+__device__ int efa_cuda_init_rdma_read_wr(void *wr_buf, uint16_t wr_id, uint32_t rkey, uint64_t remote_addr);
+
+__device__ int efa_cuda_init_rdma_write_wr(void *wr_buf, uint16_t wr_id, uint32_t rkey, uint64_t remote_addr);
+
+__device__ int efa_cuda_init_rdma_write_imm_wr(void *wr_buf, uint16_t wr_id, uint32_t rkey, uint64_t remote_addr, uint32_t imm_data);
+
+__device__ void efa_cuda_wr_set_remote(void *wr_buf, uint16_t ah, uint32_t remote_qpn, uint32_t remote_qkey);
+
+__device__ int efa_cuda_wr_set_inline_data(void *wr_buf, void *addr, size_t length);
+
+__device__ int efa_cuda_wr_set_sge(void *wr_buf, uint32_t lkey, uint64_t addr, uint32_t length);
+
+__device__ void efa_cuda_flush_sq_wrs(efa_cuda_qp *qp);
+
+__device__ int efa_cuda_start_sq_batch(efa_cuda_qp *qp, int batch_size);
+
+__device__ int efa_cuda_sq_batch_place_wr(efa_cuda_qp *qp, int index_in_batch, void *wr_buf);
+
+__device__ int efa_cuda_post_recv_wr(efa_cuda_qp *qp, uint64_t addr, uint32_t length, uint32_t lkey);
+
+__device__ void efa_cuda_flush_rq_wrs(efa_cuda_qp *qp);
+
+__device__ bool efa_cuda_is_cq_compatible(efa_cuda_cq *cq);
+
+__device__ bool efa_cuda_is_qp_compatible(efa_cuda_qp *qp);
+
+namespace efa_cuda_dp {
+
+	struct efa_cuda_cq *create_cq(struct efa_cuda_cq_attrs *attrs, uint32_t inlen);
+	void destroy_cq(efa_cuda_cq *d_cq);
+	struct efa_cuda_qp *create_qp(struct efa_cuda_qp_attrs *attrs, uint32_t inlen);
+	void destroy_qp(efa_cuda_qp *d_qp);
+	int get_version(int *major, int *minor, int *subminor);
+}
+
+#endif

CUDA/src/efa_cuda_dp_impl.cuh
@@ -1,0 +1,443 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+#ifndef EFA_CUDA_DP_IMPL_CUH
+#define EFA_CUDA_DP_IMPL_CUH
+
+#include <stdio.h>
+#include <cstring>
+#include <errno.h>
+#include <stdint.h>
+
+#include "efa_cuda_dp.h"
+#include "efa_io_defs.h"
+
+#define BIT(nr)		(1UL << (nr))
+
+#define __bf_shf(x)	(__builtin_ffsll(x) - 1)
+
+#define EFA_FIELD_GET(_mask, _reg)						\
+	({									\
+		(typeof(_mask))(((_reg) & (_mask)) >> __bf_shf(_mask));		\
+	})
+
+#define EFA_FIELD_PREP(_mask, _val)						\
+	({									\
+		((typeof(_mask))(_val) << __bf_shf(_mask)) & (_mask);		\
+	})
+
+#define BITS_PER_LONG	(8 * sizeof(long))
+
+#define GENMASK(h, l)								\
+	(((~0UL) - (1UL << (l)) + 1) & (~0UL >> (BITS_PER_LONG - 1 - (h))))
+
+#define EFA_GET(ptr, mask)							\
+	EFA_FIELD_GET(mask##_MASK, *(typeof(*ptr) volatile *)(ptr))
+
+#define EFA_SET(ptr, mask, value)						\
+	({									\
+		typeof(ptr) _ptr = ptr;				        	\
+		*_ptr = (*_ptr & ~(mask##_MASK)) |				\
+			EFA_FIELD_PREP(mask##_MASK, value);		        \
+	})
+
+#define efa_container_of(ptr, type, field)					\
+	((type *) ((char *)ptr - offsetof(type, field)))
+
+
+__device__ inline int efa_cuda_cqe_is_pending(const efa_io_cdesc_common *cqe_common, int phase)
+{
+	return EFA_GET(&cqe_common->flags, EFA_IO_CDESC_COMMON_PHASE) == phase;
+}
+
+__device__ inline efa_io_cdesc_common *efa_cuda_get_cqe(efa_cuda_cq *cq, uint32_t position)
+{
+	uint32_t index = (cq->cc + position) & cq->queue_mask;
+	return (efa_io_cdesc_common *)(cq->buf + (index * cq->entry_size));
+}
+
+__device__ inline int efa_cuda_get_cqe_phase(efa_cuda_cq *cq, uint32_t position)
+{
+	return cq->phase ^ (((cq->cc & cq->queue_mask) + position) >> cq->queue_size_shift);
+}
+
+__device__ void *efa_cuda_cq_poll(efa_cuda_cq *cq, uint32_t position)
+{
+	efa_io_cdesc_common *cqe = efa_cuda_get_cqe(cq, position);
+	int cqe_phase = efa_cuda_get_cqe_phase(cq, position);
+
+	if (efa_cuda_cqe_is_pending(cqe, cqe_phase)) {
+		__threadfence_block();
+
+		return cqe;
+	}
+	return nullptr;
+}
+__device__ int efa_cuda_cq_pop(efa_cuda_cq *cq, uint32_t amount)
+{
+	cq->phase = efa_cuda_get_cqe_phase(cq, amount);
+	cq->cc += amount;
+
+	return 0;
+}
+
+__device__ enum efa_cuda_wc_opcode efa_cuda_wc_read_opcode(void *wc_buf)
+{
+	enum efa_io_send_op_type op_type;
+	struct efa_io_cdesc_common *cqe = (struct efa_io_cdesc_common *)wc_buf;
+
+	op_type = (enum efa_io_send_op_type)EFA_GET(&cqe->flags, EFA_IO_CDESC_COMMON_OP_TYPE);
+
+	if (EFA_GET(&cqe->flags, EFA_IO_CDESC_COMMON_Q_TYPE) == EFA_IO_SEND_QUEUE) {
+		if (op_type == EFA_IO_RDMA_WRITE)
+			return EFA_CUDA_WC_RDMA_WRITE;
+
+		if (op_type == EFA_IO_RDMA_READ)
+			return EFA_CUDA_WC_RDMA_READ;
+
+		return EFA_CUDA_WC_SEND;
+	}
+
+	if (op_type == EFA_IO_RDMA_WRITE)
+		return EFA_CUDA_WC_RECV_RDMA_WITH_IMM;
+
+	return EFA_CUDA_WC_RECV;
+}
+
+__device__ bool efa_cuda_wc_is_unsolicited(void *wc_buf)
+{
+	struct efa_io_cdesc_common *cqe = (struct efa_io_cdesc_common *)wc_buf;
+
+	return EFA_GET(&cqe->flags, EFA_IO_CDESC_COMMON_UNSOLICITED);
+}
+
+__device__ uint16_t efa_cuda_wc_read_req_id(void *wc_buf)
+{
+	struct efa_io_cdesc_common *cqe = (struct efa_io_cdesc_common *)wc_buf;
+
+	return cqe->req_id;
+}
+
+__device__ uint32_t efa_cuda_wc_read_vendor_err(void *wc_buf)
+{
+	struct efa_io_cdesc_common *cqe = (struct efa_io_cdesc_common *)wc_buf;
+
+	return cqe->status;
+}
+
+__device__ bool efa_cuda_wc_has_imm(void *wc_buf)
+{
+	struct efa_io_cdesc_common *cqe = (struct efa_io_cdesc_common *)wc_buf;
+
+	return EFA_GET(&cqe->flags, EFA_IO_CDESC_COMMON_HAS_IMM);
+}
+
+__device__ uint32_t efa_cuda_wc_read_imm_data(void *wc_buf)
+{
+	struct efa_io_rx_cdesc *rcqe;
+
+	rcqe = efa_container_of(wc_buf, struct efa_io_rx_cdesc, common);
+
+	return rcqe->imm;
+}
+
+__device__ uint32_t efa_cuda_wc_read_byte_len(void *wc_buf)
+{
+	struct efa_io_cdesc_common *cqe = (struct efa_io_cdesc_common *)wc_buf;
+	struct efa_io_rx_cdesc_ex *rcqe;
+	uint32_t length;
+
+	if (EFA_GET(&cqe->flags, EFA_IO_CDESC_COMMON_Q_TYPE) != EFA_IO_RECV_QUEUE)
+		return 0;
+
+	rcqe = efa_container_of(cqe, struct efa_io_rx_cdesc_ex, base.common);
+
+	length = rcqe->base.length;
+	if (EFA_GET(&cqe->flags, EFA_IO_CDESC_COMMON_OP_TYPE) == EFA_IO_RDMA_WRITE)
+		length |= ((uint32_t)rcqe->u.rdma_write.length_hi << 16);
+
+	return length;
+}
+
+__device__ uint32_t efa_cuda_wc_read_qp_num(void *wc_buf)
+{
+	struct efa_io_cdesc_common *cqe = (struct efa_io_cdesc_common *)wc_buf;
+
+	return cqe->qp_num;
+}
+
+__device__ uint32_t efa_cuda_wc_read_src_qp(void *wc_buf)
+{
+	struct efa_io_rx_cdesc *rcqe;
+
+	rcqe = efa_container_of(wc_buf, struct efa_io_rx_cdesc, common);
+
+	return rcqe->src_qp_num;
+}
+
+__device__ uint32_t efa_cuda_wc_read_slid(void *wc_buf)
+{
+	struct efa_io_rx_cdesc *rcqe;
+
+	rcqe = efa_container_of(wc_buf, struct efa_io_rx_cdesc, common);
+
+	return rcqe->ah;
+}
+
+__device__ int efa_cuda_sq_init_wr(void *wr_buf, enum efa_io_send_op_type op_type, uint16_t wr_id)
+{
+	struct efa_io_tx_wqe *wqe = (struct efa_io_tx_wqe *)wr_buf;
+
+	memset(wqe, 0, sizeof(*wqe));
+	EFA_SET(&wqe->meta.ctrl1, EFA_IO_TX_META_DESC_META_DESC, 1);
+	EFA_SET(&wqe->meta.ctrl1, EFA_IO_TX_META_DESC_OP_TYPE, op_type);
+	EFA_SET(&wqe->meta.ctrl2, EFA_IO_TX_META_DESC_FIRST, 1);
+	EFA_SET(&wqe->meta.ctrl2, EFA_IO_TX_META_DESC_LAST, 1);
+	EFA_SET(&wqe->meta.ctrl2, EFA_IO_TX_META_DESC_COMP_REQ, 1);
+
+	wqe->meta.req_id = wr_id;
+
+	return 0;
+}
+
+__device__ void efa_cuda_set_wqe_imm_data(void *wr_buf, uint32_t imm_data)
+{
+	struct efa_io_tx_wqe *wqe = (struct efa_io_tx_wqe *)wr_buf;
+
+	wqe->meta.immediate_data = imm_data;
+	EFA_SET(&wqe->meta.ctrl1, EFA_IO_TX_META_DESC_HAS_IMM, 1);
+}
+
+__device__ void efa_cuda_set_remote_mem(struct efa_io_remote_mem_addr *remote_mem, uint32_t rkey, uint64_t remote_addr)
+{
+	remote_mem->rkey = rkey;
+	remote_mem->buf_addr_lo = remote_addr & 0xFFFFFFFF;
+	remote_mem->buf_addr_hi = remote_addr >> 32;
+}
+
+__device__ void efa_cuda_set_tx_buf(struct efa_io_tx_buf_desc *tx_buf, uint64_t addr, uint32_t lkey, uint32_t length)
+{
+	tx_buf->length = length;
+	EFA_SET(&tx_buf->lkey, EFA_IO_TX_BUF_DESC_LKEY, lkey);
+	tx_buf->buf_addr_lo = addr & 0xffffffff;
+	tx_buf->buf_addr_hi = addr >> 32;
+}
+
+__device__ int efa_cuda_init_send_wr(void *wr_buf, uint16_t wr_id)
+{
+	return efa_cuda_sq_init_wr(wr_buf, EFA_IO_SEND, wr_id);
+}
+
+__device__ int efa_cuda_init_send_imm_wr(void *wr_buf, uint16_t wr_id, uint32_t imm_data)
+{
+	int ret;
+
+	ret = efa_cuda_sq_init_wr(wr_buf, EFA_IO_SEND, wr_id);
+	if (ret)
+		return ret;
+
+	efa_cuda_set_wqe_imm_data(wr_buf, imm_data);
+
+	return 0;
+}
+
+__device__ int efa_cuda_init_rdma_read_wr(void *wr_buf, uint16_t wr_id, uint32_t rkey, uint64_t remote_addr)
+{
+	struct efa_io_tx_wqe *wqe = (struct efa_io_tx_wqe *)wr_buf;
+	int ret;
+
+	ret = efa_cuda_sq_init_wr(wr_buf, EFA_IO_RDMA_READ, wr_id);
+	if (ret)
+		return ret;
+
+	efa_cuda_set_remote_mem(&wqe->data.rdma_req.remote_mem, rkey, remote_addr);
+
+	return 0;
+}
+
+__device__ int efa_cuda_init_rdma_write_wr(void *wr_buf, uint16_t wr_id, uint32_t rkey, uint64_t remote_addr)
+{
+	struct efa_io_tx_wqe *wqe = (struct efa_io_tx_wqe *)wr_buf;
+	int ret;
+
+	ret = efa_cuda_sq_init_wr(wr_buf, EFA_IO_RDMA_WRITE, wr_id);
+	if (ret)
+		return ret;
+
+	efa_cuda_set_remote_mem(&wqe->data.rdma_req.remote_mem, rkey, remote_addr);
+
+	return 0;
+}
+
+__device__ int efa_cuda_init_rdma_write_imm_wr(void *wr_buf, uint16_t wr_id, uint32_t rkey, uint64_t remote_addr, uint32_t imm_data)
+{
+	struct efa_io_tx_wqe *wqe = (struct efa_io_tx_wqe *)wr_buf;
+	int ret;
+
+	ret = efa_cuda_sq_init_wr(wr_buf, EFA_IO_RDMA_WRITE, wr_id);
+	if (ret)
+		return ret;
+
+	efa_cuda_set_remote_mem(&wqe->data.rdma_req.remote_mem, rkey, remote_addr);
+	efa_cuda_set_wqe_imm_data(wr_buf, imm_data);
+
+	return 0;
+}
+
+__device__ void efa_cuda_wr_set_remote(void *wr_buf, uint16_t ah, uint32_t remote_qpn, uint32_t remote_qkey)
+{
+	struct efa_io_tx_wqe *wqe = (struct efa_io_tx_wqe *)wr_buf;
+
+	wqe->meta.ah = ah;
+	wqe->meta.dest_qp_num = remote_qpn;
+	wqe->meta.qkey = remote_qkey;
+}
+
+__device__ int efa_cuda_wr_set_inline_data(void *wr_buf, void *addr, size_t length)
+{
+	struct efa_io_tx_wqe *wqe = (struct efa_io_tx_wqe *)wr_buf;
+	uint8_t op_type;
+
+	if (length > sizeof(wqe->data.inline_data))
+		return -EINVAL;
+
+	op_type = EFA_GET(&wqe->meta.ctrl1, EFA_IO_TX_META_DESC_OP_TYPE);
+	if (op_type != EFA_IO_SEND)
+		return -EINVAL;
+
+	EFA_SET(&wqe->meta.ctrl1, EFA_IO_TX_META_DESC_INLINE_MSG, 1);
+	memcpy(wqe->data.inline_data, addr, length);
+	wqe->meta.length = length;
+
+	return 0;
+}
+
+__device__ int efa_cuda_wr_set_sge(void *wr_buf, uint32_t lkey, uint64_t addr, uint32_t length)
+{
+	struct efa_io_tx_buf_desc *buf;
+	struct efa_io_tx_wqe *wqe;
+	uint8_t op_type;
+
+	wqe = (struct efa_io_tx_wqe *)wr_buf;
+	wqe->meta.length = 1;
+
+	op_type = EFA_GET(&wqe->meta.ctrl1, EFA_IO_TX_META_DESC_OP_TYPE);
+	switch (op_type) {
+	case EFA_IO_SEND:
+		buf = &wqe->data.sgl[0];
+		break;
+	case EFA_IO_RDMA_READ:
+	case EFA_IO_RDMA_WRITE:
+		wqe->data.rdma_req.remote_mem.length = length;
+		buf = &wqe->data.rdma_req.local_mem[0];
+		break;
+	default:
+		return -EINVAL;
+	}
+
+	efa_cuda_set_tx_buf(buf, addr, lkey, length);
+	return 0;
+}
+
+__device__ inline int efa_cuda_get_wqe_phase(efa_cuda_wq *wq, uint32_t index_in_batch)
+{
+	return wq->phase ^ (((wq->pc & wq->queue_mask) + index_in_batch) >> wq->queue_size_shift);
+}
+
+__device__ void efa_cuda_flush_sq_wrs(efa_cuda_qp *qp)
+{
+	if (!qp->sq.wq.wqes_pending)
+		return;
+
+	qp->sq.wq.phase = efa_cuda_get_wqe_phase(&qp->sq.wq, qp->sq.wq.wqes_pending);
+	qp->sq.wq.pc += qp->sq.wq.wqes_pending;
+	qp->sq.wq.wqes_pending = 0;
+
+	__threadfence_system();
+	*qp->sq.wq.db = qp->sq.wq.pc;
+	__threadfence_system();
+}
+
+__device__ int efa_cuda_start_sq_batch(efa_cuda_qp *qp, int batch_size)
+{
+	// TODO: check free space
+
+	if (qp->sq.wq.wqes_pending + batch_size > qp->sq.wq.max_batch)
+		efa_cuda_flush_sq_wrs(qp);
+
+	qp->sq.wq.wqes_pending += batch_size;
+	return 0;
+}
+
+__device__ int efa_cuda_sq_batch_place_wr(efa_cuda_qp *qp, int index_in_batch, void *wr_buf)
+{
+	int wqe_phase = efa_cuda_get_wqe_phase(&qp->sq.wq, index_in_batch);
+	struct efa_io_tx_wqe *wqe = (struct efa_io_tx_wqe *)wr_buf;
+	uint32_t sq_desc_offset;
+	uint64_t *src;
+	uint64_t *dst;
+
+	EFA_SET(&wqe->meta.ctrl2, EFA_IO_TX_META_DESC_PHASE, wqe_phase);
+
+	src = (uint64_t *)wqe;
+	sq_desc_offset = ((qp->sq.wq.pc + index_in_batch) & qp->sq.wq.queue_mask) * sizeof(struct efa_io_tx_wqe);
+	dst = (uint64_t *)(qp->sq.wq.buf + sq_desc_offset);
+	for (int i = 0 ; i < 8 ; i++)
+		dst[i] = src[i];
+
+	return 0;
+}
+
+__device__ int efa_cuda_post_recv_wr(efa_cuda_qp *qp, uint64_t addr, uint32_t length, uint32_t lkey)
+{
+	struct efa_io_rx_desc wqe = {0};
+	uint32_t rq_desc_offset;
+
+	EFA_SET(&wqe.lkey_ctrl, EFA_IO_RX_DESC_FIRST, 1);
+	EFA_SET(&wqe.lkey_ctrl, EFA_IO_RX_DESC_LAST, 1);
+
+	EFA_SET(&wqe.lkey_ctrl, EFA_IO_RX_DESC_LKEY, lkey);
+	wqe.buf_addr_lo = addr;
+	wqe.buf_addr_hi = addr >> 32;
+	wqe.length = length;
+
+	/* Copy descriptor to RX ring */
+	rq_desc_offset = (qp->rq.wq.pc & qp->rq.wq.queue_mask) * sizeof(wqe);
+	memcpy(qp->rq.wq.buf + rq_desc_offset, &wqe, sizeof(wqe));
+
+	qp->rq.wq.pc++;
+	if (!(qp->rq.wq.pc & qp->rq.wq.queue_mask))
+		qp->rq.wq.phase++;
+
+	qp->rq.wq.wqes_pending++;
+	if (qp->rq.wq.wqes_pending == qp->rq.wq.max_batch) {
+		__threadfence_system();
+		*qp->rq.wq.db = qp->rq.wq.pc;
+
+		qp->rq.wq.wqes_pending = 0;
+	}
+
+	return 0;
+}
+
+__device__ void efa_cuda_flush_rq_wrs(efa_cuda_qp *qp)
+{
+	if (!qp->rq.wq.wqes_pending)
+		return;
+
+	__threadfence_system();
+	*qp->rq.wq.db = qp->rq.wq.pc;
+	qp->rq.wq.wqes_pending = 0;
+}
+
+__device__ bool efa_cuda_is_cq_compatible(efa_cuda_cq *cq)
+{
+	return cq->comp_mask == 0;
+}
+
+__device__ bool efa_cuda_is_qp_compatible(efa_cuda_qp *qp)
+{
+	return qp->comp_mask == 0;
+}
+
+#endif

CUDA/Makefile
@@ -1,0 +1,30 @@
+NVCC=nvcc
+NVCC_FLAGS=-G -rdc=true
+CUDA_FLAGS=-Xcompiler -fPIC
+
+SRCDIR=src
+BUILDDIR=build
+INCDIR=$(BUILDDIR)/include
+
+all: $(BUILDDIR)/libefacudadp.so headers
+
+$(BUILDDIR):
+	mkdir -p $(BUILDDIR)
+
+$(INCDIR): | $(BUILDDIR)
+	mkdir -p $(INCDIR)
+
+headers: $(INCDIR)
+	cp $(SRCDIR)/*.h $(SRCDIR)/*.cuh $(INCDIR)/
+
+$(BUILDDIR)/libefacudadp.so: $(BUILDDIR)/efa_cuda_dp.cu.o $(BUILDDIR)/efa_cuda_dp.o | $(BUILDDIR)
+	$(NVCC) $(NVCC_FLAGS) -shared -o $@ $^
+
+$(BUILDDIR)/efa_cuda_dp.cu.o: $(SRCDIR)/efa_cuda_dp.cu $(SRCDIR)/efa_cuda_dp.cuh $(SRCDIR)/efa_cuda_dp_impl.cuh | $(BUILDDIR)
+	$(NVCC) $(NVCC_FLAGS) -c $< $(CUDA_FLAGS) -o $@
+
+$(BUILDDIR)/efa_cuda_dp.o: $(SRCDIR)/efa_cuda_dp.cpp $(SRCDIR)/efa_cuda_dp.h $(SRCDIR)/efa_cuda_dp.cuh | $(BUILDDIR)
+	$(NVCC) $(NVCC_FLAGS) -c $< $(CUDA_FLAGS) -o $@
+
+clean:
+	rm -rf $(BUILDDIR)

CUDA/src/efa_cuda_dp.cpp
@@ -1,0 +1,30 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+#include "efa_cuda_dp.h"
+#include "efa_cuda_dp.cuh"
+
+struct efa_cuda_cq *efa_cuda_create_cq(struct efa_cuda_cq_attrs *attrs, uint32_t inlen)
+{
+	return efa_cuda_dp::create_cq(attrs, inlen);
+}
+
+void efa_cuda_destroy_cq(efa_cuda_cq *d_cq)
+{
+	return efa_cuda_dp::destroy_cq(d_cq);
+}
+
+struct efa_cuda_qp *efa_cuda_create_qp(struct efa_cuda_qp_attrs *attrs, uint32_t inlen)
+{
+	return efa_cuda_dp::create_qp(attrs, inlen);
+}
+
+void efa_cuda_destroy_qp(struct efa_cuda_qp *d_qp)
+{
+	efa_cuda_dp::destroy_qp(d_qp);
+}
+
+int efa_cuda_get_version(int *major, int *minor, int *subminor)
+{
+	return efa_cuda_dp::get_version(major, minor, subminor);
+}

CUDA/src/efa_cuda_dp.cu
@@ -1,0 +1,166 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+#include <stdio.h>
+#include <errno.h>
+#include <stdint.h>
+
+#include "efa_cuda_dp.cuh"
+#include "efa_cuda_dp_impl.cuh"
+#include "efa_io_defs.h"
+
+static bool is_buf_cleared(void *buf, size_t len)
+{
+	int i;
+
+	for (i = 0; i < len; i++) {
+		if (((uint8_t *)buf)[i])
+			return false;
+	}
+
+	return true;
+}
+
+#define is_ext_cleared(ptr, inlen) \
+	is_buf_cleared((uint8_t *)ptr + sizeof(*ptr), inlen - sizeof(*ptr))
+
+
+namespace efa_cuda_dp {
+
+struct efa_cuda_cq *create_cq(struct efa_cuda_cq_attrs *attrs, uint32_t inlen)
+{
+	cudaError_t cuda_err;
+
+	if (inlen > sizeof(*attrs) && !is_ext_cleared(attrs, inlen)) {
+		printf("Incompatible attributes struct\n");
+		return nullptr;
+	}
+
+	if (__builtin_popcount(attrs->num_entries) != 1) {
+		printf("CQ size must be positive power of 2\n");
+		return nullptr;
+	}
+
+	// Allocate device CQ structure
+	efa_cuda_cq *d_cq;
+	cuda_err = cudaMalloc(&d_cq, sizeof(efa_cuda_cq));
+	if (cuda_err != cudaSuccess) {
+		printf("Failed to allocate device memory for cq: %s\n",
+		       cudaGetErrorString(cuda_err));
+		return nullptr;
+	}
+
+	// Initialize and copy CQ structure
+	efa_cuda_cq h_cq = {};
+	h_cq.buf = attrs->buffer;
+	h_cq.entry_size = attrs->entry_size;
+	h_cq.num_entries = attrs->num_entries;
+	h_cq.queue_mask = attrs->num_entries - 1;
+	h_cq.queue_size_shift = __builtin_ctz(attrs->num_entries);
+	h_cq.cc = 0;
+	h_cq.phase = 1;
+
+	cuda_err = cudaMemcpy(d_cq, &h_cq, sizeof(efa_cuda_cq), cudaMemcpyHostToDevice);
+	if (cuda_err != cudaSuccess) {
+		cudaFree(d_cq);
+		printf("Failed to copy cq to device: %s\n",
+		       cudaGetErrorString(cuda_err));
+		return nullptr;
+	}
+
+	return d_cq;
+}
+
+void destroy_cq(efa_cuda_cq *d_cq)
+{
+	cudaFree(d_cq);
+}
+
+struct efa_cuda_qp *create_qp(struct efa_cuda_qp_attrs *attrs, uint32_t inlen)
+{
+	cudaError_t cuda_err;
+
+	if ((inlen > sizeof(*attrs) && !is_ext_cleared(attrs, inlen)) ||
+	    attrs->reserved) {
+		printf("Incompatible attributes struct\n");
+		return nullptr;
+	}
+
+	if (__builtin_popcount(attrs->sq_num_entries) != 1 ||
+	    __builtin_popcount(attrs->rq_num_entries) != 1) {
+		printf("SQ and RQ sizes must be positive powers of 2\n");
+		return nullptr;
+	}
+
+	// Allocate device QP structure
+	efa_cuda_qp *d_qp;
+	cuda_err = cudaMalloc(&d_qp, sizeof(efa_cuda_qp));
+	if (cuda_err != cudaSuccess) {
+		printf("Failed to allocate device memory for qp: %s\n",
+		       cudaGetErrorString(cuda_err));
+		return nullptr;
+	}
+
+	// Initialize QP structure on host
+	efa_cuda_qp h_qp = {};
+
+	// Initialize SQ
+	h_qp.sq.wq.buf = attrs->sq_buffer;
+	h_qp.sq.wq.db = attrs->sq_doorbell;
+	h_qp.sq.wq.max_wqes = attrs->sq_num_entries;
+	h_qp.sq.wq.max_batch = attrs->sq_max_batch;
+	h_qp.sq.wq.queue_mask = attrs->sq_num_entries - 1;
+	h_qp.sq.wq.queue_size_shift = __builtin_ctz(attrs->sq_num_entries);
+	h_qp.sq.wq.wqes_pending = 0;
+	h_qp.sq.wq.wqes_posted = 0;
+	h_qp.sq.wq.wqes_completed = 0;
+	h_qp.sq.wq.pc = 0;
+	h_qp.sq.wq.phase = 0;
+	// TODO: get from args or delete:
+	h_qp.sq.max_inline_data = 32;
+	h_qp.sq.max_rdma_sges = 2;
+
+	// Initialize RQ
+	h_qp.rq.wq.buf = attrs->rq_buffer;
+	h_qp.rq.wq.db = attrs->rq_doorbell;
+	h_qp.rq.wq.max_wqes = attrs->rq_num_entries;
+	h_qp.rq.wq.max_batch = attrs->rq_num_entries;
+	h_qp.rq.wq.queue_mask = attrs->rq_num_entries - 1;
+	h_qp.rq.wq.queue_size_shift = __builtin_ctz(attrs->rq_num_entries);
+	h_qp.rq.wq.wqes_pending = 0;
+	h_qp.rq.wq.wqes_posted = 0;
+	h_qp.rq.wq.wqes_completed = 0;
+	h_qp.rq.wq.pc = 0;
+	h_qp.rq.wq.phase = 1;
+
+	// Copy QP structure to device
+	cuda_err = cudaMemcpy(d_qp, &h_qp, sizeof(efa_cuda_qp), cudaMemcpyHostToDevice);
+	if (cuda_err != cudaSuccess) {
+		cudaFree(d_qp);
+		printf("Failed to copy qp to device: %s\n",
+		       cudaGetErrorString(cuda_err));
+		return nullptr;
+	}
+
+	return d_qp;
+}
+
+void destroy_qp(efa_cuda_qp *d_qp)
+{
+	if (d_qp) {
+		cudaFree(d_qp);
+	}
+}
+
+int get_version(int *major, int *minor, int *subminor) {
+	if (!major || !minor || !subminor)
+		return -EINVAL;
+
+	*major = EFA_CUDA_DP_VERSION_MAJOR;
+	*minor = EFA_CUDA_DP_VERSION_MINOR;
+	*subminor = EFA_CUDA_DP_VERSION_SUBMINOR;
+
+	return 0;
+}
+
+}

CUDA/src/efa_cuda_dp.h
@@ -1,0 +1,109 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+#ifndef EFA_CUDA_DP_H
+#define EFA_CUDA_DP_H
+
+#include <stddef.h>
+#include <stdint.h>
+#include <cuda_runtime.h>
+
+#include "efa_io_defs.h"
+
+#define EFA_CUDA_DP_VERSION_MAJOR 0
+#define EFA_CUDA_DP_VERSION_MINOR 0
+#define EFA_CUDA_DP_VERSION_SUBMINOR 1
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+enum efa_cuda_wc_opcode {
+	EFA_CUDA_WC_SEND,
+	EFA_CUDA_WC_RDMA_WRITE,
+	EFA_CUDA_WC_RDMA_READ,
+/*
+ * Set value of EFA_CUDA_WC_RECV so consumers can test if a completion is a
+ * receive by testing (opcode & EFA_CUDA_WC_RECV).
+ */
+	EFA_CUDA_WC_RECV                  = 1 << 7,
+	EFA_CUDA_WC_RECV_RDMA_WITH_IMM,
+};
+
+struct efa_cuda_cq {
+	uint64_t comp_mask;
+	uint32_t entry_size;
+	uint32_t num_entries;
+	uint32_t queue_mask;
+	uint32_t queue_size_shift;
+	uint32_t cc;
+	int phase;
+	uint8_t *buf;
+	uint32_t *db;
+};
+
+struct efa_cuda_wq {
+	uint32_t max_sge;
+	uint32_t max_wqes;
+	uint32_t queue_mask;
+	uint32_t queue_size_shift;
+	uint32_t max_batch;
+	uint32_t wqes_pending;
+	uint32_t wqes_posted;
+	uint32_t wqes_completed;
+	uint32_t pc;
+	int phase;
+	uint8_t *buf;
+	uint32_t *db;
+};
+
+struct efa_cuda_rq {
+	struct efa_cuda_wq wq;
+};
+
+struct efa_cuda_sq {
+	struct efa_cuda_wq wq;
+	uint32_t max_inline_data;
+	uint32_t max_rdma_sges;
+};
+
+struct efa_cuda_qp {
+	uint64_t comp_mask;
+	struct efa_cuda_sq sq;
+	struct efa_cuda_rq rq;
+};
+
+struct efa_cuda_cq_attrs {
+	uint64_t comp_mask;
+	uint64_t flags;
+	uint8_t *buffer;
+	uint32_t num_entries;
+	uint32_t entry_size;
+};
+
+struct efa_cuda_qp_attrs {
+	uint64_t comp_mask;
+	uint64_t flags;
+	uint8_t *sq_buffer;
+	uint8_t *rq_buffer;
+	uint32_t *sq_doorbell;
+	uint32_t *rq_doorbell;
+	uint32_t sq_num_entries;
+	uint32_t sq_entry_size;
+	uint32_t sq_max_batch;
+	uint32_t rq_num_entries;
+	uint32_t rq_entry_size;
+	uint32_t reserved;
+};
+
+struct efa_cuda_cq *efa_cuda_create_cq(struct efa_cuda_cq_attrs *attrs, uint32_t inlen);
+void efa_cuda_destroy_cq(struct efa_cuda_cq *d_cq);
+struct efa_cuda_qp *efa_cuda_create_qp(struct efa_cuda_qp_attrs *attrs, uint32_t inlen);
+void efa_cuda_destroy_qp(struct efa_cuda_qp *d_qp);
+int efa_cuda_get_version(int *major, int *minor, int *subminor);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif

commit 44aab7b48fac826b5d3ba5de7a68ae2ed6f9934d

cuda: Add req id param to post recv
Add a req_id parameter to efa cuda post recv wr, allowing callers to tag
receive work requests with a request ID.

Fixes: 25967086c680 ("Add initial code")
Reviewed-by: Yonatan Nachum <ynachum@amazon.com>
Reviewed-by: Michael Margolin <mrgolin@amazon.com>
Signed-off-by: Tom Sela <tomsela@amazon.com>

CUDA/src/efa_cuda_dp.cuh
@@ -56,7 +56,7 @@ __device__ int efa_cuda_start_sq_batch(efa_cuda_qp *qp, int batch_size);
 
 __device__ int efa_cuda_sq_batch_place_wr(efa_cuda_qp *qp, int index_in_batch, void *wr_buf);
 
-__device__ int efa_cuda_post_recv_wr(efa_cuda_qp *qp, uint64_t addr, uint32_t length, uint32_t lkey);
+__device__ int efa_cuda_post_recv_wr(efa_cuda_qp *qp, uint16_t req_id, uint64_t addr, uint32_t length, uint32_t lkey);
 
 __device__ void efa_cuda_flush_rq_wrs(efa_cuda_qp *qp);
 

CUDA/src/efa_cuda_dp_impl.cuh
@@ -388,7 +388,7 @@ __device__ int efa_cuda_sq_batch_place_wr(efa_cuda_qp *qp, int index_in_batch, v
 	return 0;
 }
 
-__device__ int efa_cuda_post_recv_wr(efa_cuda_qp *qp, uint64_t addr, uint32_t length, uint32_t lkey)
+__device__ int efa_cuda_post_recv_wr(efa_cuda_qp *qp, uint16_t req_id, uint64_t addr, uint32_t length, uint32_t lkey)
 {
 	struct efa_io_rx_desc wqe = {0};
 	uint32_t rq_desc_offset;
@@ -400,6 +400,7 @@ __device__ int efa_cuda_post_recv_wr(efa_cuda_qp *qp, uint64_t addr, uint32_t le
 	wqe.buf_addr_lo = addr;
 	wqe.buf_addr_hi = addr >> 32;
 	wqe.length = length;
+	wqe.req_id = req_id;
 
 	/* Copy descriptor to RX ring */
 	rq_desc_offset = (qp->rq.wq.pc & qp->rq.wq.queue_mask) * sizeof(wqe);